921283373a1d20cf57fb23008ada121663d54e68
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-11 12:43:24 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psExceptions/psFastWebOrders/psFastWebOrdersModule.js
+++
@@ -1,6 +0,0 @@
-﻿"use strict";
-
-angular.module('psFastWebOrders', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.bootstrap', 'ui.grid.edit', 'angular-confirm', 'angularjs-datetime-picker', 'angular-growl', 'ui.grid.expandable', 'MessageLogModule', 'ui.grid.cellNav', 'ui.grid.treeView', 'ngIdle']);
-
-
-
