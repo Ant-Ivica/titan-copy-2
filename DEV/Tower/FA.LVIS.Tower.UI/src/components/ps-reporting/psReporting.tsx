d9b62520f720a2a1b8864c05b197a52a2ecd6dc5
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-11 12:38:09 +0200

    edit_file_with_context_snippets: psReporting.tsx Input File: Tower/FA.LVIS.Tower.UI/src/components/ps-reporting/psReporting.tsx Max Tries: 6 Arguments Provided: path_to_file, context_snippets, target, edit_prompt
    Git Stats:
    Files Changed: 1
    Line Insertions: 3
    Line Deletions: 2

--- DEV/Tower/FA.LVIS.Tower.UI/src/components/ps-reporting/psReporting.tsx
+++ DEV/Tower/FA.LVIS.Tower.UI/src/components/ps-reporting/psReporting.tsx
@@ -1,5 +1,5 @@
  
-import React, { useEffect } from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import Modal from 'react-modal'; 
 import { toast, ToastContainer } from 'react-toastify'; 
 import 'react-toastify/dist/ReactToastify.css'; 
@@ -21,6 +21,7 @@ import {
     setFilterSection, 
     setDisableDate 
 } from './modules/psReportingActions'; 
+import ReportingRowDetail from './ext-modules/psReporting/reporting-row-detail'; 
 
 const ReportingComponent = () => { 
     const data = useSelector(state => state.data); 
@@ -109,7 +110,7 @@ const ReportingComponent = () => {
             ) : ( 
                 <ul> 
                     {data.map(item => ( 
-                        <li key={item.id}>{item.name}</li> // Adjust according to actual data structure 
+                        <ReportingRowDetail key={item.id} item={item} /> 
                     ))} 
                 </ul> 
             )} 
