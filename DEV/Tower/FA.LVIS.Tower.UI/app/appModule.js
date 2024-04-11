89ebcf35f1d8caedf59bd45d949d9ba7c88fbba6
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-11 16:28:37 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/app/appModule.js
+++
@@ -1,7 +0,0 @@
-﻿"use strict";
-
-angular.module("app", ["ngRoute", "psFramework", "ngStorage", "ngMessages", "ngCookies", "ng.deviceDetector", "angular-autogrow"]);
-
-
-
-
--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psSecurity/psSecurityModule.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-
-angular.module('psSecurity', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.bootstrap', 'ui.grid.edit', 'isteven-multi-select','angular-confirm','angular-growl']);
