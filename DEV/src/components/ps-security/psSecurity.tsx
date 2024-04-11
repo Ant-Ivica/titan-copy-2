33ad01638e144aa8f47aacac60dbf77790b8f4d6
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-11 11:05:23 +0200

    edit_file_with_context_snippets: psSecurity.tsx Input File: src/components/ps-security/psSecurity.tsx Max Tries: 6 Arguments Provided: path_to_file, context_snippets, target, edit_prompt
    Git Stats:
    Files Changed: 1
    Line Insertions: 3
    Line Deletions: 8

--- DEV/src/components/ps-security/psSecurity.tsx
+++ DEV/src/components/ps-security/psSecurity.tsx
@@ -1,34 +1,29 @@
- 
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as psSecurityService from '../services/psSecurity.service';
 import { setScope, setRootScope } from './modules/psSecurityStore';
-
+import ReportingRowDetail from '../components/psReporting/ReportingRowDetail';
 function MyComponent() {
   const scope = useSelector(state => state.psSecurity.scope);
   const rootScope = useSelector(state => state.psSecurity.rootScope);
   const dispatch = useDispatch();
-
   useEffect(() => {
     // Example of replacing direct API call with service call
     psSecurityService.getScopeData()
       .then(data => dispatch(setScope(data)))
       .catch(error => console.error('Failed to fetch scope data', error));
-
     psSecurityService.getRootScopeData()
       .then(data => dispatch(setRootScope(data)))
       .catch(error => console.error('Failed to fetch root scope data', error));
-
     // Additional logic to run on component mount can be added here
   }, [dispatch]);
-
   return (
     <div>
       {/* Render your component UI here */}
       <p>Scope: {JSON.stringify(scope)}</p>
       <p>Root Scope: {JSON.stringify(rootScope)}</p>
+      <ReportingRowDetail scope={scope} rootScope={rootScope} />
     </div>
   );
 }
-
 export default MyComponent;
