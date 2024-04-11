0dee35f1c031d50d6d4d5baa4e5b782f303f736d
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-11 12:04:51 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMappings/psFastGabMappings/psFastGabmappingsModule.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-
-angular.module('psfastgabmappings', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.bootstrap', 'ui.grid.edit', 'ui.grid.pagination', 'ui.grid.exporter', 'angular-confirm']);
--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMenu/psMenuModule.js
+++
@@ -1,5 +0,0 @@
-﻿"use strict";
-
-angular.module("psMenu", ["ngAnimate", "ui.bootstrap","ngIdle"]);
-
-angular.module("psSubMenu", ["ngAnimate", "ui.bootstrap"]);
