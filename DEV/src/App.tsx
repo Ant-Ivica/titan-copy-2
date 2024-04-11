20350b7cd57c082057a5b3b05219d72817e142b8
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-11 16:11:52 +0200

    rename_file: index.html Input File: src/index.html Max Tries: 1 Arguments Provided: path_to_file, new_file_name
    Git Stats:
    Files Changed: 2
    Line Insertions: 46
    Line Deletions: 72

--- DEV/src/App.tsx
+++ DEV/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tower v1.0</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css">
+<link href="Content/bootstrap.min.css" rel="stylesheet" />
+<link href="Content/ui-grid.css" rel="stylesheet" />
+<link href="Content/font-awesome.min.css" rel="stylesheet" />
+<link href="towercss" rel="stylesheet" />
+<script src="towerjqscripts" type="text/javascript"></script>
+<script src="towerangularscripts" type="text/javascript"></script>
+<script src="towerscripts" type="text/javascript"></script>
+<script src="towersubmenuscripts" type="text/javascript"></script>
+</head>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<body class="container-fluid">
+<div antiforgerytoken></div>
+<div>
+    <div class="alert alert-danger" style={{ display: errors.unauthorized ? 'block' : 'none' }}>
+        <div class="error"><i class="fa fa-lg fa-ban"> {errors.unauthorized}</i></div>
     </div>
-  );
-}
+</div>
 
-export default App;
+<div style={{ display: !errors.unauthorized ? 'block' : 'none' }}>
+    <div>
+        <Framework iconFile="images/FirstAmericanLogo.png" currentUser={currentUser} tenantName={tenantName}>
+            <Menu>
+                <MenuItem label="Home" route="/dashboard" icon="fa-dashboard" />
+                <MenuItem label="Reporting" route="/reporting" icon="fa-line-chart" />
+                <MenuItem label="Mapping Tables" route="/Customermappings" icon="fa-cogs" />
+                <MenuItem label="Auditing" route="/auditing" show={(activityRight === 'Admin') || (activityRight === 'SuperAdmin')} icon="fa-bars" />
+                <MenuItem label="Exceptions" route="/businessexception" icon="fa-exclamation-triangle" />
+                <MenuItem label="Security" route="/security" show={(activityRight === 'Admin') || (activityRight === 'SuperAdmin')} icon="fa-lock" />
+                <MenuItem label="Utilities" route="/confirmservicerequest" show={(activityRight === 'SuperAdmin')} icon="fa-wrench" />
+                <MenuItem label="Access Request" route="/AccessRequest" show={canManageAccessReq} icon="fa-key" />
+                <MenuItem label="Help" route="/help" icon="fa-info-circle" onClick={openPopupWindow} />
+            </Menu>
+        </Framework>
+    </div>
+</div>
+</body>
+</html>
