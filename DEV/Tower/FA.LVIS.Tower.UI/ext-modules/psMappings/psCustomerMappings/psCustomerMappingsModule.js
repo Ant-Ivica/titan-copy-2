ec76489ebef1881f7586e5f6f89a06f43c12e339
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-12 12:06:15 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMappings/psCustomerMappings/psCustomerMappingsModule.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-
-angular.module('psCustomerMappings', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.bootstrap', 'ui.grid.edit', 'ui.grid.pagination', 'ui.grid.exporter', 'angular-growl', 'checklist-model', 'ui.grid.cellNav', 'angular-confirm']);
