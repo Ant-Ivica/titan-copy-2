fd33f67aa9c18f65ee63bb43bdda1663f06f4297
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-12 15:45:31 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/DBScripts/Release30/01_Tables/01.AlterCredentialTableCustomerId.sql
+++
@@ -1,6 +0,0 @@
-if not exists (select * from information_schema.columns where table_name =  'Credential' and COLUMN_NAME = 'CustomerId')
-	Begin
-		ALTER TABLE Credential
-		ADD customerid INTEGER,
-		CONSTRAINT FK_Credential_Customer FOREIGN KEY(customerid) REFERENCES Customer(customerid)
-	End
--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psTerminalLogInformations/psTerminalLogInformationModule.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-
-angular.module('psTerminalLogInformations', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.bootstrap', 'ui.grid.edit', 'angular-confirm', 'angularjs-datetime-picker', 'angular-growl', 'ui.grid.expandable', 'ui.grid.cellNav', 'MessageLogModule']);
