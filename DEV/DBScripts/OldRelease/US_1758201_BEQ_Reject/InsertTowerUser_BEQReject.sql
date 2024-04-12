6ecd230886d66286d6aa189bea1b42425bc2c99c
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-12 15:10:57 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/DBScripts/OldRelease/US_1758201_BEQ_Reject/InsertTowerUser_BEQReject.sql
+++
@@ -1,5 +0,0 @@
-if not exists(select id from dbo.[Tower.Users] where name='BEQ Reject Job')
-Begin
-insert into dbo.[Tower.Users](Id,Name,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,EmployeeId,UserName,IsActive,TenantId)
-values('6D0ED039-EC9A-43DD-854C-A32299F5F16C','BEQ Reject Job',null,null,null,null,null,null,null,null,null,null,0,'BEQ Reject Job',1,3)
-end
