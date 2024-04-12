dfd5d49fbd7f7b8b9bad8eb58faeccb623d3ae8b
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-12 13:38:46 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psAuditing/psAuditingModule.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-
-angular.module('psAuditing', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.bootstrap', 'ui.grid.edit', 'isteven-multi-select', 'angular-confirm', 'ui.grid.cellNav', 'angularjs-datetime-picker']);
