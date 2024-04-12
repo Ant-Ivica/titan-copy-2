7f65054b5faaadd40500fa3f95cbb158dd858930
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-12 14:53:13 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMappings/psFastFilePreferenceMappings/psFastFilePreferenceMappingsModule.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-
-angular.module('psFastFilePreferenceMappings', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.bootstrap', 'ui.grid.edit', 'ui.grid.pagination', 'ui.grid.exporter', 'angular-growl', 'isteven-multi-select', 'ui.grid.cellNav', 'angular-confirm']);
--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMappings/psOutDocumentMappings/psOutDocumentMappingsModule.js
+++
@@ -1,4 +0,0 @@
-﻿"use strict";
-
-angular.module('psOutDocumentMappings', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.bootstrap', 'ui.grid.edit', 'ui.grid.pagination', 'ui.grid.exporter', 'isteven-multi-select', 'angular-confirm', 'angular-growl']);
-
