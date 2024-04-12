87ec253db3f643a318fdcea4497a09ab270afa14
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-12 15:32:42 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMappings/psCategoryMappings/psCategoryMappingsModule.js
+++
@@ -1,4 +0,0 @@
-﻿"use strict";
-
-angular.module('psCategoryMappings', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.bootstrap', 'ui.grid.edit', 'ui.grid.pagination', 'ui.grid.exporter', 'ui.grid.cellNav', 'angular-growl']);
-
