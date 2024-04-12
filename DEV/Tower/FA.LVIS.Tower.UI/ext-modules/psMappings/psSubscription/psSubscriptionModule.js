1267ffbfdfa98b18da230f75f5c6eca960d3054c
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-12 11:06:37 +0200

    create_react_app Max Tries: 1 Arguments Provided:
    Git Stats:
    Files Changed: 1814
    Line Insertions: 18318
    Line Deletions: 788578

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMappings/psSubscription/psSubscriptionModule.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-
-angular.module('psSubscription', ['ngAnimate', 'ui.grid', 'ui.grid.grouping', 'ui.grid.moveColumns', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.bootstrap', 'ui.grid.edit', 'ui.grid.pagination', 'ui.grid.exporter', 'angular-growl', 'checklist-model', 'angular-confirm']);
